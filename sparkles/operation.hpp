75e6e59ba07cf9c1c72b7d4fbb14d52fa83eef5b
Eric Hopper <hopper@omnifarious.org>
2011-11-15 09:25:14 -0800

    Fixed up some interfaces and design bugs found in testing.

--- sparkles/operation.hpp
+++ sparkles/operation.hpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <utility>
 #include <type_traits>
+#include <algorithm>
 
 namespace sparkles {
 
@@ -19,20 +20,10 @@ class operation_base : public ::std::enable_shared_from_this<operation_base>
    operation_base(const operation_base &) = delete;
    operation_base &operator =(const operation_base &) = delete;
 
-   operation_base(operation_base &&other) = default;
-   operation_base &operator =(operation_base &&other) = default;
+   operation_base(operation_base &&other) = delete;
+   operation_base &operator =(operation_base &&other) = delete;
 
    virtual ~operation_base();
-
-   template <class InputIterator>
-   operation_base(InputIterator begin,
-                  const InputIterator &end)
-        : finished_(false), dependencies_(begin, end)
-   {
-      for (auto dependency : dependencies_) {
-         dependency->add_dependent(shared_from_this());
-      }
-   }
 
    bool finished() const { return finished_; }
 
@@ -41,10 +32,34 @@ class operation_base : public ::std::enable_shared_from_this<operation_base>
 
    void dependency_finished(const opbase_ptr_t &dependency);
 
+   //! Register given object as a dependent with all its dependencies.
+   //
+   // This method should be harmless for anybody to call at any time.  But bad
+   // things will happen if the constructing static method does not call it.
+   static void register_as_dependent(const opbase_ptr_t &op) {
+      op->for_each_dependency([op](const opbase_ptr_t &dep) {
+            dep->add_dependent(op);
+         });
+   }
+
  protected:
+   template <class InputIterator>
+   operation_base(InputIterator begin,
+                  const InputIterator &end)
+        : finished_(false), dependencies_(begin, end)
+   {
+   }
+
    void set_finished();
 
    void cancel_dependency(const opbase_ptr_t &dependency);
+
+   template <class UnaryFunction>
+   UnaryFunction for_each_dependency(UnaryFunction f) const {
+      return ::std::for_each(dependencies_.begin(),
+                             dependencies_.end(),
+                             f);
+   }
 
  private:
    typedef ::std::weak_ptr<operation_base> weak_opbase_ptr_t;
@@ -53,21 +68,28 @@ class operation_base : public ::std::enable_shared_from_this<operation_base>
    ::std::unordered_set<opbase_ptr_t> dependencies_;
    ::std::unordered_map<operation_base *, weak_opbase_ptr_t> dependents_;
 
-   virtual
-   void i_dependency_finished(const opbase_ptr_t &dependency) = 0;
+   virtual void i_dependency_finished(const opbase_ptr_t &dependency) = 0;
 };
 
 inline operation_base::~operation_base() = default;
 
+//! An operation that returns a result.
+//
+// ResultType must be moveable or copyable. If it isn't moveable, it will be
+// copied.
 template <class ResultType>
 class operation : public operation_base
 {
  public:
    typedef typename ::std::remove_const<ResultType>::type result_t;
 
-   operation(operation &&other) = default;
-   operation &operator =(operation &&other) = default;
+   bool is_exception() const { return exception_ != nullptr; }
+   bool is_error() const { return is_error_; }
 
+   result_t result();
+
+ protected:
+   //! Construct an operation<ResultType> with the given set of dependencies
    template <class InputIterator>
    operation(InputIterator dependencies_begin,
              const InputIterator &dependencies_end)
@@ -75,20 +97,8 @@ class operation : public operation_base
           is_error_(false), exception_(nullptr)
    {
    }
-   operation(const ::std::initializer_list< opbase_ptr_t > &lst)
-        : operation_base(lst.begin(), lst.end()),
-          is_error_(false), exception_(nullptr)
-   {
-   }
 
-   bool is_exception() const { return exception_ != nullptr; }
-   bool is_error() const { return is_error_; }
-
-   result_t &&result();
-
- protected:
-   void set_result(result_t &&result);
-   void set_result(const result_t &result);
+   void set_result(result_t result);
    void set_result(::std::exception_ptr exception);
    void set_result(::std::error_code error);
 
@@ -97,10 +107,12 @@ class operation : public operation_base
    result_t result_;
    ::std::exception_ptr exception_;
    ::std::error_code error_;
+
+   virtual void i_dependency_finished(const opbase_ptr_t &dependency) = 0;
 };
 
 template <class ResultType>
-typename operation<ResultType>::result_t &&operation<ResultType>::result()
+typename operation<ResultType>::result_t operation<ResultType>::result()
 {
    if (!is_valid_) {
       throw invalid_result("attempt to get an already fetched result.");
@@ -119,31 +131,15 @@ typename operation<ResultType>::result_t &&operation<ResultType>::result()
 }
 
 template <class ResultType>
-void operation<ResultType>::set_result(operation<ResultType>::result_t &&result)
+void operation<ResultType>::set_result(operation<ResultType>::result_t result)
 {
    if (is_valid_) {
       throw invalid_result("Attempt to set a result that's already been set.");
    } else {
-      result_ = result;
+      result_ = ::std::move(result);
       is_valid_ = true;
       is_error_ = false;
       delete exception_;
-      exception_ = nullptr;
-      set_finished();
-   }
-}
-
-template <class ResultType>
-void operation<ResultType>::set_result(
-   const operation<ResultType>::result_t &result
-   )
-{
-   if (is_valid_) {
-      throw invalid_result("Attempt to set a result that's already been set.");
-   } else {
-      result_ = result;
-      is_valid_ = true;
-      is_error_ = false;
       exception_ = nullptr;
       set_finished();
    }
