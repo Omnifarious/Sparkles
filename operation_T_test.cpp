96a08f7a5e57f5d6e788c542bfbee14ca37cec1d
Eric Hopper <hopper@omnifarious.org>
2011-12-12 06:39:15 -0800

    The first, very simple operation<T> test case.

--- operation_T_test.cpp
+++ operation_T_test.cpp
@@ -3,9 +3,9 @@
 #include <boost/test/unit_test.hpp>
 
 #include <vector>
-#include <algorithm>
 #include <memory>
 #include <utility>
+#include <exception>
 
 namespace sparkles {
 namespace test {
@@ -15,25 +15,24 @@ BOOST_AUTO_TEST_SUITE(operation_T_test)
 typedef ::std::vector< ::std::string> finishedq_t;
 
 template <typename ResultType>
-class nodep_op : public operation<ResultType> {
-   struct privclass {
-   };
+class base_testop : public operation<ResultType> {
    typedef operation<ResultType> baseclass_t;
 
  public:
-   typedef ::std::shared_ptr<nodep_op<ResultType>> ptr_t;
    typedef typename baseclass_t::result_t result_t;
    typedef typename baseclass_t::opbase_ptr_t opbase_ptr_t;
 
-   nodep_op(const privclass &,
-            ::std::string name, finishedq_t &finishedq, bool *deleted)
-        : operation_base({}),
+   virtual ~base_testop() { if (deleted_) { *deleted_ = true; } }
+
+   const ::std::string &name() const { return name_; }
+
+ protected:
+   base_testop(::std::string name, finishedq_t &finishedq, bool *deleted,
+               const ::std::initializer_list<opbase_ptr_t> &lst)
+        : baseclass_t(lst.begin(), lst.end()),
           name_(::std::move(name)), finishedq_(finishedq), deleted_(deleted)
    {
    }
-   virtual ~nodep_op() { if (deleted_) { *deleted_ = true; } }
-
-   ::std::string name() const { return name_; }
 
    void set_result(result_t result) {
       finishedq_.push_back(name_);
@@ -49,6 +48,31 @@ class nodep_op : public operation<ResultType> {
       finishedq_.push_back(name_);
       baseclass_t::set_result(::std::move(error));
    }
+
+ private:
+   const ::std::string name_;
+   finishedq_t &finishedq_;
+   bool * const deleted_;
+};
+
+template <typename ResultType>
+class nodep_op : public base_testop<ResultType> {
+   struct privclass {
+   };
+   typedef base_testop<ResultType> baseclass_t;
+
+ public:
+   typedef ::std::shared_ptr<nodep_op<ResultType> > ptr_t;
+   typedef typename baseclass_t::result_t result_t;
+   typedef typename baseclass_t::opbase_ptr_t opbase_ptr_t;
+
+   nodep_op(const privclass &,
+            ::std::string name, finishedq_t &finishedq, bool *deleted)
+        : baseclass_t(::std::move(name), finishedq, deleted, {})
+   {
+   }
+
+   using baseclass_t::set_result;
 
    static ptr_t
    create(const ::std::string &name, finishedq_t &finishedq, bool *deleted)
@@ -62,39 +86,61 @@ class nodep_op : public operation<ResultType> {
    }
 
  private:
-   virtual void i_dependency_finished(const opbase_ptr_t &dependency)
+   virtual void i_dependency_finished(const opbase_ptr_t &)
    {
       throw ::std::runtime_error("This object should have no dependencies.");
    }
-
-   const ::std::string name_;
-   finishedq_t &finishedq_;
-   bool * const deleted_;
 };
 
 template <typename ResultType, typename Arg1_t, typename Arg2_t>
-class op_add : public operation<ResultType>
+class op_add : public base_testop<ResultType>
 {
    struct privclass {
    };
-   typedef operation<ResultType> baseclass_t;
+   typedef base_testop<ResultType> baseclass_t;
 
  public:
-   typedef ::std::shared_ptr<op_add<ResultType, Arg1_t, Arg2_t>> ptr_t;
-   typedef ::std::shared_ptr<operation<Arg1_t>> arg1_ptr_t;
-   typedef ::std::shared_ptr<operation<Arg2_t>> arg2_ptr_t;
+   typedef ::std::shared_ptr<op_add<ResultType, Arg1_t, Arg2_t> > ptr_t;
+   typedef ::std::shared_ptr<operation<Arg1_t> > arg1_ptr_t;
+   typedef ::std::shared_ptr<operation<Arg2_t> > arg2_ptr_t;
    typedef typename baseclass_t::result_t result_t;
    typedef typename baseclass_t::opbase_ptr_t opbase_ptr_t;
 
    op_add(const privclass &,
           const ::std::string &name, finishedq_t &finishedq, bool *deleted,
           arg1_ptr_t arg1, arg2_ptr_t arg2)
-        : operation_base({arg1, arg2}),
-          name_(name), finishedq_(finishedq), deleted_(deleted),
+        : baseclass_t(name, finishedq, deleted, {arg1, arg2}),
           arg1_(::std::move(arg1)), arg2_(::std::move(arg2))
    {
-      bool found_error = false;
       if (arg1_->finished()) {
+         this->i_dependency_finished(arg1_);
+      }
+      if (!this->finished() && arg2_->finished()) {
+         this->i_dependency_finished(arg2_);
+      }
+   }
+
+   static ptr_t
+   create(const ::std::string &name, finishedq_t &finishedq, bool *deleted,
+          arg1_ptr_t arg1, arg2_ptr_t arg2)
+   {
+      typedef op_add<ResultType, Arg1_t, Arg2_t> me_t;
+      ptr_t newthunk{
+         ::std::make_shared<me_t>(privclass{}, name, finishedq, deleted,
+                                  arg1, arg2)
+            };
+      register_as_dependent(newthunk);
+      return ::std::move(newthunk);
+   }
+
+ private:
+   arg1_ptr_t arg1_;
+   arg2_ptr_t arg2_;
+
+   virtual void i_dependency_finished(const opbase_ptr_t &dependency)
+   {
+      bool found_error = false;
+      if (dependency == arg1_) {
          if (arg1_->is_exception()) {
             found_error = true;
             set_result(arg1_->exception());
@@ -102,7 +148,7 @@ class op_add : public operation<ResultType>
             found_error = true;
             set_result(arg1_->error());
          }
-      } else if (arg2_->finished()) {
+      } else if (dependency == arg2_) {
          if (arg2_->is_exception()) {
             found_error = true;
             set_result(arg2_->exception());
@@ -110,126 +156,53 @@ class op_add : public operation<ResultType>
             found_error = true;
             set_result(arg2_->error());
          }
+      } else {
+         throw ::std::runtime_error("A dependency I don't have finished.");
       }
-      if (!found_error && arg1_->finished() && arg2_->finished()) {
-         set_result(arg1_->result() + arg2_->result());
-      }
-      if (this->finished()) {
-         arg1_->reset();
-         arg2_->reset();
-      }
-   }
-
-   virtual ~op_add() { if (deleted_) { *deleted_ = true; } }
-
-   void set_result(result_t result) {
-      finishedq_.push_back(name_);
-      baseclass_t::set_result(::std::move(result));
-   }
-   void set_result(::std::exception_ptr exception)
-   {
-      finishedq_.push_back(name_);
-      baseclass_t::set_result(::std::move(exception));
-   }
-   void set_result(::std::error_code error)
-   {
-      finishedq_.push_back(name_);
-      baseclass_t::set_result(::std::move(error));
-   }
-
- private:
-   const ::std::string name_;
-   finishedq_t &finishedq_;
-   bool * const deleted_;
-   arg1_ptr_t arg1_;
-   arg2_ptr_t arg2_;
-};
-
-#if 0
-template <typename ResultType>
-class  : public operation<ResultType> {
-   struct privclass {
-   };
-
- public:
-   opthunk(const privclass &,
-           const ::std::string &name, finishedq_t &finishedq, bool *deleted,
-           const ::std::initializer_list<opbase_ptr_t> &lst)
-        : operation_base(lst.begin(), lst.end()),
-          name_(name), finishedq_(finishedq), deleted_(deleted),
-          numdeps_(lst.size()),
-          depsfinished_(::std::count_if(lst.begin(), lst.end(),
-                                        [](const opbase_ptr_t &dep) -> bool {
-                                           return dep->finished();
-                                        }
-                           ))
-   {
-   }
-   virtual ~opthunk() { if (deleted_) { *deleted_ = true; } }
-
-   ::std::string name() const { return name_; }
-
-   void set_finished() {
-      finishedq_.push_back(name_);
-      operation_base::set_finished();
-   }
-
-   void remove_dependency(const opbase_ptr_t &dependency) {
-      if (dependency != nullptr) {
-         operation_base::remove_dependency(dependency);
-         if (!dependency->finished()) {
-            if (++depsfinished_ >= numdeps_) {
-               BOOST_CHECK_EQUAL(depsfinished_, numdeps_);
-               bool all_finished = true;
-               for_each_dependency(
-                  [&all_finished](const opbase_ptr_t &dep) -> void {
-                     if (!dep->finished()) all_finished = false;
-                  });
-               BOOST_CHECK(all_finished);
-               BOOST_CHECK(!finished());
-               if (!finished()) {
-                  set_finished();
-               }
-            }
+      if (found_error && arg1_->finished() && arg2_->finished()) {
+         try {
+            result_t result = arg1_->result() + arg2_->result();
+            this->set_result(::std::move(result));
+         } catch (...) {
+            this->set_result(::std::current_exception());
          }
       }
-   }
-
-   static ::std::shared_ptr<opthunk>
-   create(const ::std::string &name, finishedq_t &finishedq, bool *deleted,
-          const ::std::initializer_list<opbase_ptr_t> &lst)
-   {
-      ::std::shared_ptr<opthunk> newthunk{
-         ::std::make_shared<opthunk>(privclass{}, name, finishedq, deleted, lst)
-            };
-      register_as_dependent(newthunk);
-      return newthunk;
-   }
-
- private:
-   virtual void i_dependency_finished(const opbase_ptr_t &dependency) {
-      BOOST_REQUIRE_MESSAGE(dependency->finished(),
-                            "\"" << name_
-                            << "\"->i_dependency_finished called with "
-                            "unfinished dependency.");
-      if (++depsfinished_ >= numdeps_) {
-         BOOST_CHECK_EQUAL(depsfinished_, numdeps_);
-         bool all_finished = true;
-         for_each_dependency([&all_finished](const opbase_ptr_t &dep) -> void {
-               if (!dep->finished()) all_finished = false;
-            });
-         BOOST_CHECK(all_finished);
-         set_finished();
+      if (this->finished()) {
+         arg1_.reset();
+         arg2_.reset();
       }
    }
-
- private:
-   const ::std::string name_;
-   finishedq_t &finishedq_;
-   bool * const deleted_;
-   unsigned int numdeps_, depsfinished_;
 };
-#endif
+
+template <typename Arg1_t, typename Arg2_t>
+typename op_add<
+   decltype(::std::declval<operation<Arg1_t> >().result() +             \
+            ::std::declval<operation<Arg1_t> >().result()),
+   Arg1_t,
+   Arg2_t>::ptr_t
+make_add(::std::string name, finishedq_t &finishedq, bool *deleted,
+         typename operation<Arg1_t>::ptr_t arg1,
+         typename operation<Arg2_t>::ptr_t arg2)
+{
+   typedef op_add< decltype(::std::declval<operation<Arg1_t> >().result() +  \
+                            ::std::declval<operation<Arg1_t> >().result()),
+                   Arg1_t,
+                   Arg2_t> result_t;
+   return result_t::create(name, finishedq, deleted, arg1, arg2);
+}
+
+BOOST_AUTO_TEST_CASE( construct_empty )
+{
+   auto nested = []() {
+      finishedq_t finishedq;
+      operation<int>::ptr_t arg1(nodep_op<int>::create("arg1",
+                                                       finishedq, nullptr));
+      operation<int>::ptr_t arg2(nodep_op<int>::create("arg2",
+                                                       finishedq, nullptr));
+      auto adder = make_add<int, int>("adder", finishedq, nullptr, arg1, arg2);
+   };
+   BOOST_CHECK_NO_THROW(nested());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
