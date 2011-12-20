cff8e9c2158c681c3b280404e49f48aade0d77c5
Eric Hopper <hopper@omnifarious.org>
2011-12-20 03:42:25 -0800

    Refactored operation<T> so operation<void> does something sensible.
    And as a bonus, a whole ton of code that doesn't depend on the result type
    gets moved into a .cpp file so there isn't a separate version for each
    result type, and it only has to be compiled once.

---
+++ operation.cpp
@@ -0,0 +1,81 @@
+#include <sparkles/operation.hpp>
+#include <sparkles/errors.hpp>
+#include <exception>
+#include <system_error>
+
+namespace sparkles {
+
+void operation<void>::result()
+{
+   if (!is_valid()) {
+      throw invalid_result("attempt to fetch a non-existent result.");
+   } else if (is_error()) {
+      throw ::std::system_error(error_, "Result was an error code");
+   } else if (is_exception()) {
+      ::std::rethrow_exception(exception_);
+   }
+}
+
+::std::error_code operation<void>::error() const
+{
+   if (!is_valid()) {
+      throw invalid_result("attempt to fetch a non-existent result.");
+   } else if (!is_error()) {
+      throw invalid_result("Tried to fetch error code when result isn't an error code.");
+   } else {
+      return error_;
+   }
+}
+
+::std::exception_ptr operation<void>::exception() const
+{
+   if (!is_valid()) {
+      throw invalid_result("attempt to fetch a non-existent result.");
+   } else if (is_error() || !is_exception()) {
+      throw invalid_result("Tried to fetch an exception when result isn't an exception.");
+   } else {
+      return exception_;
+   }
+}
+
+void operation<void>::set_result()
+{
+   if (is_valid() || finished()) {
+      throw invalid_result("Attempt to set a result that's already been set.");
+   } else {
+      is_valid_ = true;
+      error_ = ::std::error_code();
+      exception_ = nullptr;
+      set_finished();
+   }
+}
+
+void operation<void>::set_bad_result(::std::exception_ptr exception)
+{
+   if (exception == nullptr) {
+      throw ::std::invalid_argument("Cannot set a null exception result.");
+   } else if (is_valid() || finished()) {
+      throw invalid_result("Attempt to set a result that's already been set.");
+   } else {
+      exception_ = exception;
+      is_valid_ = true;
+      error_ = ::std::error_code();
+      set_finished();
+   }
+}
+
+void operation<void>::set_bad_result(::std::error_code error)
+{
+   if (error == ::std::error_code()) {
+      throw ::std::invalid_argument("Cannot set a no-error error result.");
+   } else if (is_valid() || finished()) {
+      throw invalid_result("Attempt to set a result that's already been set.");
+   } else {
+      error_ = error;
+      is_valid_ = true;
+      exception_ = nullptr;
+      set_finished();
+   }
+}
+
+} // namespace sparkles
--- sparkles/operation.hpp
+++ sparkles/operation.hpp
@@ -1,37 +1,26 @@
 #pragma once
 
-#include <sparkles/errors.hpp>
 #include <sparkles/operation_base.hpp>
 #include <exception>
 #include <system_error>
 
 namespace sparkles {
 
-//! An operation that returns a result.
-//
-// ResultType must be copyable and default constructable.
-//
-// A result that is an exception can only be fetched once. After it is fetched
-// the first time, the result is rendered invalid.
-//
-// An invalid result can happen even after the operation is finished in two
-// cases. First, if the result was an exception and it was already
-// fetched. Secondly, if the operation was finished without setting a
-// result. The second case is much rarer, and likely represents a programming
-// error.
-template <class ResultType>
-class operation : public operation_base
+template <class ResultType> class operation;
+
+//! An operation that returns nothing.
+template<>
+class operation<void> : public operation_base
 {
  public:
-   typedef typename ::std::remove_const<ResultType>::type result_t;
-   typedef ::std::shared_ptr<operation<ResultType> > ptr_t;
+   typedef ::std::shared_ptr<operation<void> > ptr_t;
 
    //! Is there a valid result of any kind?
    bool is_valid() const { return is_valid_; }
    //! Is the result an exception?
    bool is_exception() const { return is_valid_ && (exception_ != nullptr); }
    //! Is the result an error?
-   bool is_error() const { return is_valid_ && is_error_; }
+   bool is_error() const { return is_valid_ && (error_ != ::std::error_code()); }
 
    //! Fetch the result.
    //
@@ -43,7 +32,10 @@ class operation : public operation_base
    //
    // And if there is no result (i.e. is_valid() is false) an invalid_result
    // exception will be thrown.
-   result_t result();
+   //
+   // When the expected result is void, the function simply returns unless it
+   // would throw an exception for one of the reasons given above.
+   void result();
 
    //! Fetch the error code.
    //
@@ -61,22 +53,21 @@ class operation : public operation_base
    ::std::exception_ptr exception() const;
 
  protected:
-   //! Construct an operation<ResultType> with the given set of dependencies
+   //! Construct an operation<void> with the given set of dependencies
    template <class InputIterator>
    operation(InputIterator dependencies_begin,
              const InputIterator &dependencies_end)
         : operation_base(dependencies_begin, dependencies_end),
-          is_valid_(false), is_error_(false), exception_(nullptr)
+          is_valid_(false), exception_(nullptr)
    {
    }
 
-   void set_result(result_t result);
+   void set_result();
    void set_bad_result(::std::exception_ptr exception);
    void set_bad_result(::std::error_code error);
 
  private:
-   bool is_valid_, is_error_;
-   result_t result_;
+   bool is_valid_;
    ::std::exception_ptr exception_;
    ::std::error_code error_;
 
@@ -85,87 +76,41 @@ class operation : public operation_base
    virtual void i_dependency_finished(const opbase_ptr_t &dependency) = 0;
 };
 
+//! An operation that returns a result.
+//
+// ResultType must be copyable and default constructable.
 template <class ResultType>
-typename operation<ResultType>::result_t operation<ResultType>::result()
+class operation : public operation<void>
 {
-   if (!is_valid_) {
-      throw invalid_result("attempt to fetch a non-existent result.");
-   } else if (is_error_) {
-      throw ::std::system_error(error_, "Result was an error code");
-   } else if (exception_ != nullptr) {
-      ::std::rethrow_exception(exception_);
-   } else {
+ public:
+   typedef typename ::std::remove_const<ResultType>::type result_t;
+   typedef ::std::shared_ptr<operation<ResultType> > ptr_t;
+
+   result_t result() {
+      operation<void>::result();
       return result_;
    }
-}
 
-template <class ResultType>
-::std::error_code operation<ResultType>::error() const
-{
-   if (!is_valid_) {
-      throw invalid_result("attempt to fetch a non-existent result.");
-   } else if (!is_error_) {
-      throw invalid_result("Tried to fetch error code when result isn't an error code.");
-   } else {
-      return error_;
+ protected:
+   //! Construct an operation<ResultType> with the given set of dependencies
+   template <class InputIterator>
+   operation(InputIterator dependencies_begin,
+             const InputIterator &dependencies_end)
+        : operation<void>(dependencies_begin, dependencies_end)
+   {
    }
-}
 
-template <class ResultType>
-::std::exception_ptr operation<ResultType>::exception() const
-{
-   if (!is_valid_) {
-      throw invalid_result("attempt to fetch a non-existent result.");
-   } else if (is_error_ || (exception_ == nullptr)) {
-      throw invalid_result("Tried to fetch an exception when result isn't an exception.");
-   } else {
-      return exception_;
+   void set_result(result_t result) {
+      if (!(is_valid() || finished())) {
+         result_ = ::std::move(result);
+      }
+      operation<void>::set_result();
    }
-}
 
-template <class ResultType>
-void operation<ResultType>::set_result(operation<ResultType>::result_t result)
-{
-   if (is_valid_ || finished()) {
-      throw invalid_result("Attempt to set a result that's already been set.");
-   } else {
-      result_ = ::std::move(result);
-      is_valid_ = true;
-      is_error_ = false;
-      exception_ = nullptr;
-      set_finished();
-   }
-}
+ private:
+   result_t result_;
 
-template <class ResultType>
-void operation<ResultType>::set_bad_result(::std::exception_ptr exception)
-{
-   if (exception == nullptr) {
-      throw ::std::invalid_argument("Cannot set a null exception result.");
-   } else if (is_valid_ || finished()) {
-      throw invalid_result("Attempt to set a result that's already been set.");
-   } else {
-      exception_ = exception;
-      is_valid_ = true;
-      is_error_ = false;
-      set_finished();
-   }
-}
-
-template <class ResultType>
-void operation<ResultType>::set_bad_result(::std::error_code error)
-{
-   if (error == ::std::error_code()) {
-      throw ::std::invalid_argument("Cannot set a no-error error result.");
-   } else if (is_valid_ || finished()) {
-      throw invalid_result("Attempt to set a result that's already been set.");
-   } else {
-      error_ = error;
-      is_valid_ = true;
-      is_error_ = true;
-      exception_ = nullptr;
-      set_finished();
-   }
-}
+   virtual void i_dependency_finished(const opbase_ptr_t &dependency) = 0;
+};
 
 } // namespace teleotask
