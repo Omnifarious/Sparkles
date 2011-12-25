b9d0cd914cc949a8049923628c71dd988ee6b0aa
Eric Hopper <hopper@omnifarious.org>
2011-12-24 21:37:44 -0800

    Forward declarations for major types in sparkles.

---
+++ sparkles/forward_decls.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace sparkles {
+
+class operation_base;
+
+class semaphore;
+
+class work_queue;
+
+template <class ResultType>
+class operation;
+
+template <class ResultType>
+class remote_operation;
+
+class bad_dependency;
+
+class invalid_result;
+
+} // namespace sparkles
