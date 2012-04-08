7b1ec470e0e370c069a77ff706363ae35b1b3a3d
Eric Hopper <hopper@omnifarious.org>
2012-04-08 14:20:23 -0700

    Move documentation for STL classes to be last in the file.
    This information is not particularly relevant to anybody wanting to read
    the forward declarations file for informational purposes. It's mostly
    there to keep Doxygen quiet and make the documentation prettier.

--- sparkles/forward_decls.hpp
+++ sparkles/forward_decls.hpp
@@ -1,29 +1,5 @@
 #pragma once
 
-
-// Documentation for some C++11 STL stuff...
-
-/*! \class std::exception_ptr
- * \brief An STL class that holds pointers to thrown exceptions.
- *
- * \sa http://en.cppreference.com/w/cpp/error/exception/exception_ptr
- */
-
-/*! \class std::error_code
- * \brief A STL class for holding error codes. Part of the C++11 STL extensible
- * error code system.
- *
- * \sa http://en.cppreference.com/w/cpp/error/system_error/error_code
- */
-
-/*! \class std::system_error
- * \extends ::std::runtime_error
- * \brief An STL exception class for throwing ::std::error_code values as
- * exceptions.
- *
- * \sa http://en.cppreference.com/w/cpp/error/system_error/system_error
- */
-
 /*! \brief The namespace for the Sparkles library.
  *
  * This library is intended to implement a way to have one asynchronous result
@@ -56,3 +32,26 @@ class bad_dependency;
 class invalid_result;
 
 } // namespace sparkles
+
+// Documentation for some C++11 STL stuff...
+
+/*! \class std::exception_ptr
+ * \brief An STL class that holds pointers to thrown exceptions.
+ *
+ * \sa http://en.cppreference.com/w/cpp/error/exception/exception_ptr
+ */
+
+/*! \class std::error_code
+ * \brief A STL class for holding error codes. Part of the C++11 STL extensible
+ * error code system.
+ *
+ * \sa http://en.cppreference.com/w/cpp/error/system_error/error_code
+ */
+
+/*! \class std::system_error
+ * \extends ::std::runtime_error
+ * \brief An STL exception class for throwing ::std::error_code values as
+ * exceptions.
+ *
+ * \sa http://en.cppreference.com/w/cpp/error/system_error/system_error
+ */
