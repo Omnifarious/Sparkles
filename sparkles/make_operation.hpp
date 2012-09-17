7596a7f223b5f4b0d80b783c8fb791f5c66dba02
Eric Hopper <hopper@omnifarious.org>
2012-09-17 14:12:46 -0700

    Lurching towards a method for creating the wrapper function.

--- sparkles/make_operation.hpp
+++ sparkles/make_operation.hpp
@@ -2,8 +2,12 @@
 #include <memory>
 #include <type_traits>
 
+struct base_stub_op {
+   virtual ~base_stub_op() = default;
+};
+
 template <typename ResultType>
-struct stub_op {
+struct stub_op : public base_stub_op {
    typedef ::std::shared_ptr<stub_op<ResultType> > ptr_t;
    typedef ResultType result_type;
 
@@ -32,11 +36,36 @@ struct is_op_ptr {
 };
 
 template <typename T>
-struct transform_type
-   : ::std::conditional< is_op_ptr<T>::value, T, typename stub_op<T>::ptr_t>
+class transform_type
 {
+ public:
+   static constexpr bool passthrough = is_op_ptr<T>::value;
+   typedef typename ::std::conditional< passthrough,
+                                        T,
+                                        typename stub_op<T>::ptr_t>::type type;
+   typedef T orig_type;
+
+   transform_type(const type &o) : wrapped_(o) { }
+   transform_type(type &&o) : wrapped_(o) { }
+
+   template <typename U = T>
+   typename ::std::enable_if< ::std::is_same<U, T>::value && passthrough,
+                              orig_type>::type
+   result() {
+      return wrapped_;
+   }
+   template <typename U = T>
+   typename ::std::enable_if< ::std::is_same<U, T>::value && !passthrough,
+                              orig_type>::type
+   result() {
+      return wrapped_->result();
+   }
+
+ private:
+   type wrapped_;
 };
 
+#if 0
 template <bool PassThrough, typename T>
 class wrapper_argument;
 
@@ -59,6 +88,26 @@ class wrapper_argument<true, T> {
 
    T unwrap(T arg) { return arg; }
 };
+
+template <typename Container_T>
+void make_deplist(Container_T &output)
+{
+}
+
+template <typename Container_T, typename FirstArg, typename... Args>
+void make_deplist(
+   Container_T &output,
+   FirstArg &first_arg,
+   Args... args
+   )
+{
+   typedef decltype(first_arg) wrapper_t;
+   if (!wrapper_t::passthrough) {
+      output.insert(output.end(), first_arg);
+   }
+   make_deplist(output, args...);
+}
+#endif
 
 template <typename ResultType, typename... ArgTypes>
 ::std::function<typename transform_type<ResultType>::type(typename transform_type<ArgTypes...>::type)>
@@ -85,7 +134,6 @@ int bar(stub_op<double>::ptr_t fred)
    return fred->result() - 5;
 }
 
-#if 0
 void test()
 {
    auto tfoo = make_function(from_funcptr(foo));
@@ -96,4 +144,3 @@ void test()
    foo(nothing);
    foo(more_nothing);
 }
-#endif
