31ca5b19810a313343b0a327a5e15c829e6d0290
Eric Hopper <hopper@omnifarious.org>
2013-01-18 00:36:26 -0800

    A sort of test sample implementation of a deferred function wrapper.

--- make_operation.cpp
+++ make_operation.cpp
@@ -1,1 +1,28 @@
 #include <sparkles/make_operation.hpp>
+#include <iostream>
+
+using ::std::cerr;
+
+int a_function()
+{
+   cerr << "In a_function.\n";
+   return 5;
+}
+
+int a_function2(int arg)
+{
+   cerr << "In a_function(" << arg << ").\n";
+   return arg;
+}
+
+#if 0
+int main()
+{
+   cerr << "Here 1\n";
+   auto func1 = defer(a_function).until();
+   cerr << "Here 2\n";
+   auto func2 = defer(a_function2).until(func1);
+   cerr << "Here 3\n";
+   cerr << "func2->result() == " << func2->result() << '\n';
+}
+#endif
--- sparkles/make_operation.hpp
+++ sparkles/make_operation.hpp
@@ -1,17 +1,77 @@
 #include <functional>
 #include <memory>
 #include <type_traits>
+#include <tuple>
 
 struct base_stub_op {
-   virtual ~base_stub_op() = default;
+   typedef ::std::shared_ptr<base_stub_op> ptr_t;
+
+   virtual ~base_stub_op() noexcept(true) = default;
 };
 
 template <typename ResultType>
 struct stub_op : public base_stub_op {
+   typedef base_stub_op::ptr_t base_ptr_t;
    typedef ::std::shared_ptr<stub_op<ResultType> > ptr_t;
    typedef ResultType result_type;
 
-   ResultType result() const { return ResultType(); }
+   virtual ResultType result() const { return ResultType(); }
+
+   static ptr_t create() {
+      return ::std::make_shared<stub_op<ResultType>>();
+   }
+};
+
+template <typename ResultType>
+struct stub_const_op : public stub_op<ResultType> {
+   typedef ::std::shared_ptr<stub_const_op<ResultType> > ptr_t;
+   typedef ResultType result_type;
+
+   explicit stub_const_op(ResultType &&val)
+        : val_(::std::move(val))
+   { }
+   explicit stub_const_op(const ResultType &val)
+        : val_(val)
+   { }
+
+   ResultType result() const { return val_; }
+
+   static ptr_t create(ResultType &&val) {
+      return ::std::make_shared<stub_const_op<ResultType>>(::std::move(val));
+   }
+   static ptr_t create(const ResultType &val) {
+      return ::std::make_shared<stub_const_op<ResultType>>(val);
+   }
+
+ private:
+   const ResultType val_;
+};
+
+template <typename ResultType>
+struct stub_func_op : public stub_op<ResultType> {
+   typedef ::std::shared_ptr<stub_func_op<ResultType> > ptr_t;
+   typedef ResultType result_type;
+   typedef ::std::function<ResultType()> func_t;
+
+   explicit stub_func_op(func_t &&func)
+        : func_(::std::move(func))
+   { }
+   explicit stub_func_op(const func_t &func)
+        : func_(func)
+   { }
+   virtual ~stub_func_op() noexcept(true) { }
+
+   ResultType result() const { return func_(); }
+
+   static ptr_t create(func_t &&func) {
+      return ::std::make_shared<stub_func_op<ResultType>>(::std::move(func));
+   }
+   static ptr_t create(const func_t &func) {
+      return ::std::make_shared<stub_func_op<ResultType>>(func);
+   }
+
+ private:
+   const func_t func_;
 };
 
 template <typename T>
@@ -44,6 +104,7 @@ class transform_type
                                         T,
                                         typename stub_op<T>::ptr_t>::type type;
    typedef T orig_type;
+   typedef decltype(::std::declval<type>()->result()) base_type;
 
    transform_type(const type &o) : wrapped_(o) { }
    transform_type(type &&o) : wrapped_(o) { }
@@ -65,82 +126,72 @@ class transform_type
    type wrapped_;
 };
 
-#if 0
-template <bool PassThrough, typename T>
-class wrapper_argument;
-
-template <typename T>
-class wrapper_argument<false, T> {
+template <typename ResultType, typename FuncT, typename TupleT>
+class suspended_call {
  public:
-   static constexpr bool passthrough = false;
-   typedef T type;
-   typedef typename T::element_type::result_type orig_type;
-
-   orig_type unwrap(T arg) { return arg->result(); }
-};
-
-template <typename T>
-class wrapper_argument<true, T> {
- public:
-   static constexpr bool passthrough = true;
-   typedef T type;
-   typedef typename T::element_type::result_type orig_type;
-
-   T unwrap(T arg) { return arg; }
-};
-
-template <typename Container_T>
-void make_deplist(Container_T &output)
-{
-}
-
-template <typename Container_T, typename FirstArg, typename... Args>
-void make_deplist(
-   Container_T &output,
-   FirstArg &first_arg,
-   Args... args
-   )
-{
-   typedef decltype(first_arg) wrapper_t;
-   if (!wrapper_t::passthrough) {
-      output.insert(output.end(), first_arg);
+   explicit suspended_call(FuncT func, TupleT args)
+        : func_(::std::move(func)), args_(::std::move(args))
+   {
    }
-   make_deplist(output, args...);
-}
-#endif
+
+   // This can only be called once and will alter the state of the object so it
+   // cannot be called again.
+   ResultType operator()() {
+      typedef call_helper< ::std::tuple_size<TupleT>::value> helper_t;
+      return ::std::move(helper_t::engage(func_, args_));
+   }
+
+ private:
+   FuncT func_;
+   TupleT args_;
+
+   template <unsigned int N, unsigned int... I>
+   struct call_helper {
+      static ResultType engage(FuncT &func, TupleT &args) {
+         return ::std::move(call_helper<N - 1, N - 1, I...>::engage(func, args));
+      }
+   };
+   template <unsigned int... I>
+   struct call_helper<0, I...> {
+      static ResultType engage(FuncT &func, TupleT &args) {
+         return ::std::move(func(::std::get<I>(args).result()...));
+      }
+   };
+};
 
 template <typename ResultType, typename... ArgTypes>
-::std::function<typename transform_type<ResultType>::type(typename transform_type<ArgTypes...>::type)>
-make_function(::std::function<ResultType(ArgTypes...)>)
+class deferred {
+ public:
+   typedef typename stub_op<ResultType>::ptr_t deferred_t;
+   typedef ::std::function<ResultType(ArgTypes...)> wrapped_func_t;
+
+   explicit deferred(const wrapped_func_t &func)
+        : func_(func)
+   {
+   }
+
+   deferred_t until(const typename transform_type<ArgTypes>::type &... args) {
+      typedef ::std::tuple<transform_type<ArgTypes>...> argtuple_t;
+      argtuple_t saved_args = ::std::make_tuple(args...);
+      ::std::function<ResultType()> f{suspended_call<ResultType, wrapped_func_t, argtuple_t>(func_, ::std::move(saved_args))};
+      return stub_func_op<ResultType>::create(f);
+   }
+
+ private:
+   const wrapped_func_t func_;
+};
+
+template <typename ResultType, typename... ArgTypes>
+deferred<ResultType, ArgTypes...>
+defer(::std::function<ResultType(ArgTypes...)> func)
 {
-   return nullptr;
+   return deferred<ResultType, ArgTypes...>(func);
 }
 
 template <typename ResultType, typename... ArgTypes>
-::std::function<ResultType(ArgTypes...)>
-from_funcptr(ResultType (*func)(ArgTypes...))
+deferred<ResultType, ArgTypes...>
+defer(ResultType (*func)(ArgTypes...))
 {
-   return ::std::function<ResultType(ArgTypes...)>{func};
-}
-
-int foo(double fred)
-{
-   return int(fred) - 5;
-}
-
-
-int bar(stub_op<double>::ptr_t fred)
-{
-   return fred->result() - 5;
-}
-
-void test()
-{
-   auto tfoo = make_function(from_funcptr(foo));
-   auto tbar = make_function(from_funcptr(bar));
-   int nothing = tfoo;
-   int more_nothing = tbar;
-   tfoo = tbar;
-   foo(nothing);
-   foo(more_nothing);
+   ::std::function<ResultType(ArgTypes...)> f = func;
+   return deferred<ResultType, ArgTypes...>(::std::move(f));
 }
